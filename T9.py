1c4e38685be7b5a00c232eacfebce641adabd5ae
장영진 <jsr9874@gmail.com>
2019-06-27 16:28:38 +0900

    test codes

---
+++ T9.py
@@ -0,0 +1,8 @@
+import test7
+import sys
+import random
+import profile
+
+if __name__ == "__main__":
+    test7.myFunc()
+    print(sys.path)
---
+++ test6.py
@@ -0,0 +1,9 @@
+for i in range(0,5,1):
+    for k in range(0,i+1,1):
+        print("*", end="")
+    print()
+
+for i in range(0,4,1):
+    for j in range(4,i,-1):
+        print("*", end="")
+    print()
